3f9beae3cee2929cd1dcfe2bae185d58753696d2
chosangwoo <kajer102@gmail.com>
2023-05-20 16:50:19 +0900

    일정 관리앱 만들어보기 1회차

---
+++ todotable/lib/component/today_banner.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:todotable/const/colors.dart';
+
+class TodayBanner extends StatelessWidget {
+  final DateTime selectedDate;
+  final int count;
+
+  const TodayBanner({
+    required this.selectedDate,
+    required this.count,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = TextStyle(
+      fontWeight: FontWeight.w600,
+      color: Colors.white,
+    );
+
+    return Container(
+      color: PRIMARY_COLOR,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "${selectedDate.year}년 ${selectedDate.month}월 ${selectedDate.day}일",
+              style: textStyle,
+            ),
+            Text(
+              "$count개",
+              style: textStyle,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- todotable/lib/screen/home_screen.dart
+++ todotable/lib/screen/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:todotable/component/main_calendar.dart';
+import 'package:todotable/component/schedule_bottom_sheet.dart';
+import 'package:todotable/component/schedule_card.dart';
+import 'package:todotable/component/today_banner.dart';
+import 'package:todotable/const/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -18,6 +22,20 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: PRIMARY_COLOR,
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            isDismissible: true,
+            builder: (_) => const ScheduleBottomSheet(),
+            isScrollControlled: true,
+          );
+        },
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
       body: SafeArea(
         child: Column(
           children: [
@@ -25,6 +43,21 @@ class _HomeScreenState extends State<HomeScreen> {
               selectedDate: selectedDate,
               onDaySelected: onDaySelected,
             ),
+            const SizedBox(
+              height: 8,
+            ),
+            TodayBanner(
+              selectedDate: selectedDate,
+              count: 0,
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            const ScheduleCard(
+              startTime: 12,
+              endTime: 14,
+              content: "프로그래밍 공부",
+            )
           ],
         ),
       ),
