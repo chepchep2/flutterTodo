b7abe11ae2201128c18e2f1487f9cd627dd8f6ec
chosangwoo <kajer102@gmail.com>
2023-05-23 22:18:26 +0900

    데이터베이스 실습중

--- todotable/lib/main.dart
+++ todotable/lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:todotable/screen/home_screen.dart';
+import 'package:todotable/database/drift_database.dart';
+import 'package:get_it/get_it.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized;
 
   await initializeDateFormatting();
+
+  final database = LocalDatabase();
+
+  GetIt.I.registerSingleton<LocalDatabase>(database);
+
   runApp(
     const MaterialApp(
       home: HomeScreen(),
