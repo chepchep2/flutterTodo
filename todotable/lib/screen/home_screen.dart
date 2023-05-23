6ffacd39da52fdf5279cbcfef2b911fc7f73464e
chosangwoo <kajer102@gmail.com>
2023-05-23 23:58:20 +0900

    연습 완성

--- todotable/lib/screen/home_screen.dart
+++ todotable/lib/screen/home_screen.dart
@@ -4,6 +4,9 @@ import 'package:todotable/component/schedule_bottom_sheet.dart';
 import 'package:todotable/component/schedule_card.dart';
 import 'package:todotable/component/today_banner.dart';
 import 'package:todotable/const/colors.dart';
+import 'package:get_it/get_it.dart';
+import 'package:todotable/database/drift_database.dart';
+import 'package:todotable/component/today_banner.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -28,7 +31,9 @@ class _HomeScreenState extends State<HomeScreen> {
           showModalBottomSheet(
             context: context,
             isDismissible: true,
-            builder: (_) => const ScheduleBottomSheet(),
+            builder: (_) => ScheduleBottomSheet(
+              selectedDate: selectedDate,
+            ),
             isScrollControlled: true,
           );
         },
@@ -46,18 +51,48 @@ class _HomeScreenState extends State<HomeScreen> {
             const SizedBox(
               height: 8,
             ),
-            TodayBanner(
-              selectedDate: selectedDate,
-              count: 0,
+            StreamBuilder<List<Schedule>>(
+              stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
+              builder: (context, snapshot) {
+                return TodayBanner(
+                  selectedDate: selectedDate,
+                  count: snapshot.data?.length ?? 0,
+                );
+              },
             ),
             const SizedBox(
               height: 8,
             ),
-            const ScheduleCard(
-              startTime: 12,
-              endTime: 14,
-              content: "프로그래밍 공부",
-            )
+            Expanded(
+                child: StreamBuilder<List<Schedule>>(
+              stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
+              builder: ((context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Container();
+                }
+                return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    final schedule = snapshot.data![index];
+                    return Dismissible(
+                      key: ObjectKey(schedule.id),
+                      direction: DismissDirection.startToEnd,
+                      onDismissed: (DismissDirection direction) {
+                        GetIt.I<LocalDatabase>().removeSchedule(schedule.id);
+                      },
+                      child: Padding(
+                        padding:
+                            const EdgeInsets.only(bottom: 8, left: 8, right: 8),
+                        child: ScheduleCard(
+                            startTime: schedule.startTime,
+                            endTime: schedule.endTime,
+                            content: schedule.content),
+                      ),
+                    );
+                  },
+                );
+              }),
+            ))
           ],
         ),
       ),
