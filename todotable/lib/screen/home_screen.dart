b5ff7b1f33c7d677a41936644c8257d7230c911a
chosangwoo <kajer102@gmail.com>
2023-05-29 19:32:54 +0900

    todoList 시작

--- todotable/lib/screen/home_screen.dart
+++ todotable/lib/screen/home_screen.dart
@@ -1,107 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:todotable/component/main_calendar.dart';
-import 'package:todotable/component/schedule_bottom_sheet.dart';
-import 'package:todotable/component/schedule_card.dart';
-import 'package:todotable/component/today_banner.dart';
-import 'package:todotable/const/colors.dart';
-import 'package:get_it/get_it.dart';
-import 'package:todotable/database/drift_database.dart';
-import 'package:todotable/component/today_banner.dart';
+import 'package:todotable/component/main_list.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  DateTime selectedDate = DateTime.utc(
-    DateTime.now().year,
-    DateTime.now().month,
-    DateTime.now().day,
-  );
+  List todoList = [
+    ["투두 타이틀", false, "투두 설명을 추가할 수 있습니다"],
+    ["스토리라인 작업하기", false, "투두 설명은 작성 시 60%로 설정합니다"],
+  ];
+
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      todoList[index][1] = !todoList[index][1];
+    });
+  }
+
+  // void createNewTodo() {
+  //   showDialog(
+  //     context: context,
+  //     builder: (context) {
+  //       return const MainAdd();
+  //     },
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: PRIMARY_COLOR,
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            isDismissible: true,
-            builder: (_) => ScheduleBottomSheet(
-              selectedDate: selectedDate,
-            ),
-            isScrollControlled: true,
+      backgroundColor: Colors.white,
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: createNewTodo,
+      //   child: const Icon(Icons.add),
+      // ),
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return MainList(
+            todoName: todoList[index][0],
+            todoCompleted: todoList[index][1],
+            todoSub: todoList[index][2],
+            onChanged: (value) => checkBoxChanged(value, index),
           );
         },
-        child: const Icon(
-          Icons.add,
-        ),
-      ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            MainCalendar(
-              selectedDate: selectedDate,
-              onDaySelected: onDaySelected,
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            StreamBuilder<List<Schedule>>(
-              stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
-              builder: (context, snapshot) {
-                return TodayBanner(
-                  selectedDate: selectedDate,
-                  count: snapshot.data?.length ?? 0,
-                );
-              },
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Expanded(
-                child: StreamBuilder<List<Schedule>>(
-              stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
-              builder: ((context, snapshot) {
-                if (!snapshot.hasData) {
-                  return Container();
-                }
-                return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    final schedule = snapshot.data![index];
-                    return Dismissible(
-                      key: ObjectKey(schedule.id),
-                      direction: DismissDirection.startToEnd,
-                      onDismissed: (DismissDirection direction) {
-                        GetIt.I<LocalDatabase>().removeSchedule(schedule.id);
-                      },
-                      child: Padding(
-                        padding:
-                            const EdgeInsets.only(bottom: 8, left: 8, right: 8),
-                        child: ScheduleCard(
-                            startTime: schedule.startTime,
-                            endTime: schedule.endTime,
-                            content: schedule.content),
-                      ),
-                    );
-                  },
-                );
-              }),
-            ))
-          ],
-        ),
       ),
     );
-  }
-
-  void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
-    setState(() {
-      this.selectedDate = selectedDate;
-    });
   }
 }
