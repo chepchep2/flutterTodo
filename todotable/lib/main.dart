d96fe122b99a921a42410438635cbbe51563afbb
chosangwoo <kajer102@gmail.com>
2023-05-29 20:44:23 +0900

    다시 시작

--- todotable/lib/main.dart
+++ todotable/lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:todotable/screen/home_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: HomeScreen(),
-    ),
+    const MaterialApp(),
   );
 }
