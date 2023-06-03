ff6d21eb26dc58e4051bd99816b3e8e825048df9
chosangwoo <kajer102@gmail.com>
2023-06-03 14:36:11 +0900

    MainBottom 빼고 MainList에 다같이 적음

--- todotable/lib/component/main_bottom.dart
+++ todotable/lib/component/main_bottom.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
 
-class MainBottom extends StatelessWidget {
+class MainBottom extends StatefulWidget {
   const MainBottom({super.key});
 
   @override
+  State<MainBottom> createState() => _MainBottomState();
+}
+
+class _MainBottomState extends State<MainBottom> {
+  final _textController = TextEditingController();
+  List todos = [];
+  String input = "";
+  @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Container(
-      child: const TextField(
-        decoration: InputDecoration(hintText: "새로운 투두 추가하기"),
+    void _handleSubmitted(String text) {
+      // _textController.clear();
+      print(text);
+    }
+
+    return Container(
+      child: Row(
+        children:
+            // <Widget> 있어도 되고 없어도 되는 것 같음 용도는??
+            [
+          Container(
+            child: IconButton(
+                onPressed: () => _handleSubmitted(_textController.text),
+                icon: const Icon(Icons.add)),
+          ),
+          Expanded(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration:
+                  const InputDecoration.collapsed(hintText: "새로운 투두 추가하기"),
+            ),
+          ),
+        ],
       ),
-    ));
+    );
   }
 }
