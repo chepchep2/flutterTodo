301a31264a1f913e66ec9c5eb9107073411a83b8
chosangwoo <kajer102@gmail.com>
2023-06-10 00:10:37 +0900

    새로운 브랜치(데이터베이스 공부용)입니다.

--- todotable/lib/component/main_list.dart
+++ todotable/lib/component/main_list.dart
@@ -38,6 +38,7 @@ class _MainListState extends State<MainList> {
 
   @override
   Widget build(BuildContext context) {
+    print("새로운 브랜치입니다.");
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
       child: Column(
